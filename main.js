b9bf3087e9ea62882083ef60b1410778419f0407
Yuchan <mikuplayer123@gmail.com>
2020-05-16 02:57:56 +0900

    JS Navbar menu click

--- main.js
+++ main.js
@@ -9,3 +9,20 @@ function changeNavBgColor() {
     ? navbar.classList.add("navbar--dark")
     : navbar.classList.remove("navbar--dark");
 }
+
+//Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector(".navbar__menu");
+
+navbarMenu.addEventListener("click", scrollToSection);
+
+function scrollToSection(e) {
+  const target = e.target;
+  const link = target.dataset.link;
+  const scroll = document.querySelector(link);
+
+  if (link) {
+    scroll.scrollIntoView({
+      behavior: "smooth",
+    });
+  }
+}
