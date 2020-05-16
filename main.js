2996f36d3dbabab7c70b5c87dc44101961fd7aa0
Yuchan <mikuplayer123@gmail.com>
2020-05-16 17:02:09 +0900

    fix toggle btn position & add js toggle

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 //Make NavBar transparent when it is on the top
 const navbar = document.getElementById("navbar");
+const navbarToggle = document.querySelector(".navbar__toggle-btn");
 const navbarHeight = navbar.getBoundingClientRect().height;
 
 document.addEventListener("scroll", changeNavBgColor);
 
 function changeNavBgColor() {
-  window.scrollY > navbarHeight
-    ? navbar.classList.add("navbar--dark")
-    : navbar.classList.remove("navbar--dark");
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add("navbar--dark");
+    navbarToggle.classList.add("navToggle--margin");
+  } else {
+    navbar.classList.remove("navbar--dark");
+    navbarToggle.classList.remove("navToggle--margin");
+  }
 }
 
 //Handle scrolling when tapping on the navbar menu and contact me button
